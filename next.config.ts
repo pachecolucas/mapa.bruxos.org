bbc4eeb0602f3c10ec7df76a04307279aef30cb7
Lucas Pacheco Teixeira <lucas@eliti.com.br>
2026-06-10 11:42:10 -0300

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   serverExternalPackages: ["sweph"],
+  outputFileTracingIncludes: {
+    // ajuste a chave para a rota que realmente usa sweph.
+    // ex: se for /api/mapa, use "/api/mapa". O "/**" pega tudo.
+    "/**": ["./node_modules/sweph/**/*.node", "./node_modules/sweph/build/**", "./node_modules/sweph/prebuilds/**"],
+  },
 };
 
 export default nextConfig;
